4ee7ce9f2426f5edc7971b83b5be343d08c20b24
jrmensah <jerilynmensah@gmail.com>
2017-10-18 10:25:26 -0400

    Created first name, last name, address, phone number and email methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,23 @@ public class Main {
 
         System.out.println("Enter person's first name:");
         person.setFirstName(keyboard.nextLine());
-        System.out.print("You entered"+" "+person.getFirstName()+" "+"as the person's name");
-
+        System.out.print("You entered"+" "+person.getFirstName()+" "+"as the person's first name");
+        System.out.println("");
+        System.out.println("Enter person's last name:");
+        person.setLastName(keyboard.nextLine());
+        System.out.print("You entered" +" "+person.getLastName()+" "+"as the person's last name");
+        System.out.println("");
+        System.out.println("Enter the person's address:");
+        person.setAddress(keyboard.nextLine());
+        System.out.print("You entered"+ " " +person.getAddress()+" "+"as the person's address");
+        System.out.println("");
+        System.out.println("Enter the person's phone number:");
+        person.setPhoneNumber(keyboard.nextLine());
+        System.out.print("You entered"+ " "+person.getPhoneNumber()+" "+"as the person's phone number");
+        System.out.println("");
+        System.out.println("Enter the person's email address");
+        person.setEmail(keyboard.nextLine());
+        System.out.print("You entered"+ " "+person.getEmail()+" "+"as the person's email address");
 
 
     }
